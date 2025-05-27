448562a3181befd271cd62af5c5fdd139e3483a9
Sam Colby <sam.colby@paddle.com>
2025-05-27 11:19:29 +0100

    fix: Improve loading skeleton

--- src/components/product-details-skeleton.tsx
+++ src/components/product-details-skeleton.tsx
@@ -2,15 +2,13 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export function ProductDetailsSkeleton() {
   return (
-    <div className="flex items-center gap-4">
-      <Skeleton className="w-12 h-12 rounded-lg" />
+    <div className="flex flex-col gap-3 px-3">
+      <Skeleton className="h-12 w-full rounded-lg" />
       <div className="space-y-2">
-        <Skeleton className="h-5 w-32" />
-        <div className="space-y-1">
-          <Skeleton className="h-4 w-24" />
-          <Skeleton className="h-3 w-28" />
-        </div>
+        <Skeleton className="h-4 w-32" />
+        <Skeleton className="h-4 w-24" />
       </div>
+      <Skeleton className="h-14 w-full rounded-lg lg:h-48" />
     </div>
   );
 }
